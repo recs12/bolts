6f8e5ba694029fc59e854c0b1a705b256515b473
slimane rechdi <slimane.rechdi@gmail.com>
2020-01-30 13:28:06 -0500

    cleaning folder

--- compile.bat
+++ compile.bat
@@ -3,11 +3,8 @@
 :: So it can be added to the user custom theme in solidedge. 
 ipyc.exe /main:./bolts/__main__.py ^
 ./bolts/Interop.SolidEdge.dll ^
-./bolts/api.py ./bullet/holes.py ^
-./bolts/equivalences.py ^
-./bolts/customs.py ^
 /embed ^
-/out:bolts_macro_64x_0-0-0 ^
+/out:bolts_macro_recs_64x_0-0-0 ^
 /platform:x64 ^
 /standalone ^
 /target:exe ^
