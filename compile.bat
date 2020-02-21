09a0fbd4850c12530940b430022f71b0bb84940b
slimane rechdi <slimane.rechdi@gmail.com>
2020-02-21 15:20:14 -0500

    0.0.3: compile.bat

--- compile.bat
+++ compile.bat
@@ -1,10 +1,11 @@
 :: app name: bolts
 :: Set this file for compiling the executable of the macro.
 :: So it can be added to the user custom theme in solidedge. 
-ipyc.exe /main:./bolts/__main__.py ^
+ipyc.exe /main:./bolts/__main__.py ./bolts/api.py ./bolts/cad_convertor.py ./bolts/nuts.py ./bolts/screws.py ./bolts/washers.py ./bolts/sources.py ^
+./Hardware/. ^
 ./bolts/Interop.SolidEdge.dll ^
 /embed ^
-/out:bolts_macro_recs_64x_0-0-0 ^
+/out:bolts_macro_recs_64x_0-0-3 ^
 /platform:x64 ^
 /standalone ^
 /target:exe ^
